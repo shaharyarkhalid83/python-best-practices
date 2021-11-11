323bdc97995048cf33a5693fa0a5cd553e72d036
shaharyarkhalid83 <49206425+shaharyarkhalid83@users.noreply.github.com>
2021-11-11 14:52:51 -0600

    Update pizza_histogram.py

--- pizza_histogram.py
+++ pizza_histogram.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 
-o = [("pepperoni", 2), ("hawaiian", 7), ("pepperoni",8), ("cheese", 2), ("hawaiian", 20), ("sausage", 5), ("cheese", 10), ("bacon", 2)]
+dictionary = [("pepperoni", 2), ("hawaiian", 7), ("pepperoni",8), ("cheese", 2), ("hawaiian", 20), ("sausage", 5), ("cheese", 10), ("bacon", 2)]
 
 
 '''
 '''
-d = {}
-for i in o:
-    if i[0] in d.keys():
-        d[i[0]] += i[1]
+result = {}
+for item in dictionary:
+    if item[0] in result.keys():
+        result[item[0]] += i[1]
     else:
 
-        d[i[0]] = i[1]
+        result[item[0]] = item[1]
 
-for k, v in d.items():
+for k, v in result.items():
     print("{}: {}".format(k, v))
 
 
-plt.bar(list(d.keys()), d.values())
+plt.bar(list(result.keys()), result.values())
 plt.show()
